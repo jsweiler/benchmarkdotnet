0be68602672c4be0d5ac779c422aacaa9807c3aa
Jeff Weiler <jeff@clearstreamsoftware.com>
2024-08-10 20:07:56 -0400

    add concatenate sample

---
+++ src/Performance.Concatenation/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace Performance.Concatenation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press Enter to start...");
+            Console.ReadKey();
+
+            int limit = 100000;
+            string result = ConcatenateNumbers2(limit);
+            Console.WriteLine($"Length of concatenated string: {result.Length}");
+        }
+
+        static string ConcatenateNumbers(int limit)
+        {
+            string result = "";
+            for (int i = 1; i <= limit; i++)
+            {
+                result += i.ToString();
+            }
+            return result;
+        }
+
+        static string ConcatenateNumbers2(int limit)
+        {
+            var stringBuilder = new StringBuilder();
+            for (int i = 1; i <= limit; i++)
+            {
+                stringBuilder.Append(i);
+            }
+            return stringBuilder.ToString();
+        }
+
+    }
+}
